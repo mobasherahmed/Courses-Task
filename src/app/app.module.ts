a3f9fa0ad9c297ebc9f1126e801d3aa618872aa7
ahmedmobasher <ahmedmobasher00@gmail.com>
2021-04-28 04:15:44 +0200

    convert psd into html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NZ_I18N } from "ng-zorro-antd/i18n";
+import { en_US } from "ng-zorro-antd/i18n";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { HeaderComponent } from "./header/header.component";
+import { CoursesComponent } from "./courses/courses.component";
+import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
+import { NzInputModule } from "ng-zorro-antd/input";
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, CoursesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzCheckboxModule,
+    NzInputModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
